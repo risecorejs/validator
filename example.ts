e41cbc781fb07926464ddca8a701c8cbbd1e2e4f
Abdulsalam Amenov <amenov.abdulsalam@gmail.com>
2022-08-24 18:33:03 +0600

    update to ts

--- example.ts
+++ example.ts
@@ -1,18 +1,14 @@
 import validator from './index'
 
-main().then((errors) => {
+validator(
+  {
+    name: 'Abdulsalam',
+    email: 'amenov.abdulsalam@gmail.com'
+  },
+  {
+    name: 'required|string|max:200',
+    email: 'required|email|max:200'
+  }
+).then((errors) => {
   console.log(errors)
 })
-
-function main() {
-  return validator(
-    {
-      name: 'Abdulsalam',
-      email: 'amenov.abdulsalam@gmail.com'
-    },
-    {
-      name: 'required|string|max:200',
-      email: 'required|email|max:200'
-    }
-  )
-}
